0bc25eba85f41bc03a61f46c8b36c14ec4b43839
Alexander Korostin <lexkrstn@gmail.com>
2022-08-06 22:03:09 +0300

    feat: purity and board filters work

--- components/triple-filters/board-filter.tsx
+++ components/triple-filters/board-filter.tsx
@@ -1,21 +1,35 @@
-import React, { ReactElement } from 'react';
+import React, { FC, useMemo, useState } from 'react';
+import { BOARD_A, BOARD_G, BOARD_P } from '../../interfaces/constants';
 import styles from './board-filter.module.scss';
 
-export default function BoardFilter(): ReactElement {
+interface BoardFilterProps {
+  onChange?: (bitmask: number) => void;
+}
+
+const BoardFilter: FC<BoardFilterProps> = ({ onChange }) => {
+  const [boards, setBoards] = useState(BOARD_G | BOARD_A | BOARD_P);
+  const clickHandlers = [BOARD_G, BOARD_A, BOARD_P].map(board => () => {
+    setBoards(boards ^ board);
+    onChange && onChange(boards ^ board);
+  });
   return (
     <div className={styles.host}>
-      <input type="hidden" name="board" id="board" value="111" />
+      <input type="hidden" name="board" value={boards} />
       <ul>
-        <li className="selected">
-          <a className="js-tooltip" href="javascript:;" title="General" data-position="bottom">G</a>
+        <li className={boards & BOARD_G ? styles.selected : ''}>
+          <button onClick={clickHandlers[0]} title="General">G</button>
         </li>
-        <li className="selected">
-          <a className="js-tooltip" href="javascript:;" title="Anime / Manga" data-position="bottom">A</a>
+        <li className={boards & BOARD_A ? styles.selected : ''}>
+          <button onClick={clickHandlers[1]} title="Anime / Manga">A</button>
         </li>
-        <li className="selected">
-          <a className="js-tooltip" href="javascript:;" title="People" data-position="bottom">P</a>
+        <li className={boards & BOARD_P ? styles.selected : ''}>
+          <button onClick={clickHandlers[2]} title="People">P</button>
         </li>
       </ul>
     </div>
   );
-}
+};
+
+BoardFilter.displayName = 'BoardFilter';
+
+export default React.memo(BoardFilter);
--- components/triple-filters/purity-filter.tsx
+++ components/triple-filters/purity-filter.tsx
@@ -1,21 +1,56 @@
-import React, { ReactElement } from 'react';
+import React, { FC, ReactElement, useState } from 'react';
+import { PURITY_NSFW, PURITY_SFW, PURITY_SKETCHY } from '../../interfaces/constants';
 import styles from './purity-filter.module.scss';
 
-export default function PurityFilter(): ReactElement {
+interface PurityFilterProps {
+  nsfwDisabled?: boolean;
+  onChange?: (bitmask: number) => void;
+}
+
+const PurityFilter: FC<PurityFilterProps> = ({ onChange, nsfwDisabled }) => {
+  const [purities, setPurities] = useState(PURITY_SFW | PURITY_SKETCHY);
+  const clickHandlers = [PURITY_SFW, PURITY_SKETCHY, PURITY_NSFW].map(purity => () => {
+    setPurities(purities ^ purity);
+    onChange && onChange(purities ^ purity);
+  });
   return (
     <div className={styles.host}>
-      <input type="hidden" name="purity" id="purity" value="110"/>
+      <input type="hidden" name="purity" value={purities} />
       <ul>
-        <li className="selected">
-          <a className="js-tooltip" href="javascript:;" title="Clean wallpaper" data-position="bottom">SFW</a>
+        <li className={purities & PURITY_SFW ? styles.selected : ''}>
+          <button
+            onClick={clickHandlers[0]}
+            title="Clean wallpaper"
+          >
+            SFW
+          </button>
         </li>
-        <li className="selected">
-          <a className="js-tooltip" href="javascript:;" title="Wallpaper with soft/erotic poses, blood, etc." data-position="bottom">SKETCHY</a>
+        <li className={purities & PURITY_SKETCHY ? styles.selected : ''}>
+          <button
+            onClick={clickHandlers[1]}
+            title="Wallpaper with soft/erotic poses, blood, etc."
+          >
+            SKETCHY
+          </button>
         </li>
-        <li>
-          <a className="js-tooltip" href="javascript:;" title="Wallpaper with visible boobs, testicles/vagina, gore, etc" data-position="bottom-right">NSFW</a>
+        <li className={purities & PURITY_NSFW ? styles.selected : ''}>
+          <button
+            onClick={nsfwDisabled ? undefined : clickHandlers[2]}
+            disabled={nsfwDisabled}
+            title="Wallpaper with visible boobs, testicles/vagina, gore, etc"
+          >
+            NSFW
+          </button>
         </li>
       </ul>
     </div>
   );
-}
+};
+
+PurityFilter.displayName = 'PurityFilter';
+
+PurityFilter.defaultProps = {
+  nsfwDisabled: false,
+};
+
+export default React.memo(PurityFilter);
